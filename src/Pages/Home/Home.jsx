8e07697fd323fdb49a56451f59e196ae6070aee5
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-09-26 18:10:14 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../../Component/Hero";
+import Navber from "../../Component/Navber";
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            <Hero></Hero>
+        </div>
+    );
+};
+
+export default Home;
